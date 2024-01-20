11655f52d6884440d1378b27c8f7f0849ba31625
satish <satishnarayana07@gmail.com>
2024-01-20 09:43:58 +0530

    First selenium committed

--- FirstSelenium.java
+++ FirstSelenium.java
@@ -3,6 +3,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class FirstSelenium {
 
 
@@ -11,7 +15,7 @@ public class FirstSelenium {
 		
 		public void chromeBrowser() throws Exception {
 		
-		System.setProperty("webdriver.chrome.driver", "C:\\ChromeDriver\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "E:\\chromedriver\\chromedriver.exe");
 	   driver=new ChromeDriver();
 		
        driver.get("https:\\youtube.com\\satishkakimukkala");
@@ -25,7 +29,7 @@ public class FirstSelenium {
 		
 		public void firefoxBrowser() throws InterruptedException {
 			
-			System.setProperty("webdriver.gecko.driver", "C:\\FirefoxDriver\\geckodriver.exe");
+			//System.setProperty("webdriver.gecko.driver", "E:\\geckodriver\\geckodriver.exe");
 			driver=new FirefoxDriver();
 			driver.get("https://google.com");
 			Thread.sleep(5000);
@@ -38,7 +42,7 @@ public class FirstSelenium {
 		
 public void edgeBrowser() throws InterruptedException {
 			
-			System.setProperty("webdriver.edge.driver", "C:\\EdgeDriver\\msedgedriver.exe");
+			System.setProperty("webdriver.edge.driver", "E:\\EdgeDriver\\msedgedriver.exe");
 			driver=new EdgeDriver();
 			driver.get("https://gmail.com");
 			Thread.sleep(5000);
@@ -47,13 +51,32 @@ public void edgeBrowser() throws InterruptedException {
 			System.out.println(driver.getTitle());
 			driver.close();
 		}
-		
+
 public static void main(String[] args) throws Exception {
 	
 	FirstSelenium fs= new FirstSelenium();
-	fs.chromeBrowser();
-	fs.firefoxBrowser();
-	fs.edgeBrowser();
+	int ch=0;
+	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	System.out.println("Enter your choice of browser");
+	System.out.println("1.chrome");
+	System.out.println("2.firefox");
+	System.out.println("3.edge");
+	ch=Integer.parseInt(br.readLine());
+
+	switch(ch)
+	{
+		case 1:
+		        fs.chromeBrowser();
+				break;
+		case 2: fs.firefoxBrowser();
+		        break;
+		case 3: fs.edgeBrowser();
+		        break;
+		default: return;
+	}
+
+
+
 
 }
 }
